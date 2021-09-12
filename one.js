fa4bf2bde0cd9f03e858e5105d797087eebc8268
Nikul <9328217188nikul@gmail.comgit config --global Nikul9.email 9328217188nikul@gmail.com>
2021-09-12 21:39:49 +0530

    nikul00

--- one.js
+++ one.js
@@ -1,12 +1,14 @@
 const nikul = () => {
     
-    console.log("nikul");
+   var a = 0
 } 
 const jeck  = () => {
-    var a = "NIKUL" 
+    var s = "dasda"
+
     console.log("jeasd");
 }
 function asd () {
     var y = 9
+    console.log('ASAS');
     console.log("NIKUL");
 }
